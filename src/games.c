cb4166f894ee0e8d5016734d141c9f79170bd40d
feelosoff <kostia_k@bk.ru>
2018-04-22 19:21:00 +0300

    add upper bound of max edges

--- src/games.c
+++ src/games.c
@@ -40,6 +40,8 @@
 #include "igraph_types_internal.h"
 #include "config.h"
 
+#include <stdint.h>
+#include <limits.h>
 #include <math.h>
 
 typedef struct {
@@ -3578,14 +3580,15 @@ int igraph_static_fitness_game(igraph_t *graph, igraph_integer_t no_of_edges,
         if (VECTOR(*fitness_out)[i] != 0 && VECTOR(*fitness_in)[i] != 0)
             nodes++;
       }
-      max_no_of_edges = ((long int) outnodes) * innodes - (loops ? 0 : nodes);
+      max_no_of_edges = min((int64_t)INT32_MAX, ((int64_t) outnodes) * innodes - (loops ? 0 : nodes));
     } else {
       nodes = 0;
       for (i=0; i < no_of_nodes; i++) {
         if (VECTOR(*fitness_out)[i] != 0)
           nodes++;
       }
-      max_no_of_edges = loops ? nodes*((long int)nodes+1)/2 : nodes*((long int)nodes-1)/2;
+      max_no_of_edges = loops ? min((int64_t)INT32_MAX, nodes*((int64_t)nodes+1)/2) :
+                                min((int64_t)INT32_MAX, nodes*((int64_t)nodes-1)/2);
     }
     if (no_of_edges > max_no_of_edges)
       IGRAPH_ERROR("Too many edges requested", IGRAPH_EINVAL);
